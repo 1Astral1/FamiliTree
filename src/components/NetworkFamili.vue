7a8e46bdc9a42ad2ac0f1f61ff434716cb6632be
1Astral1 <yyyjagernautyyy@mail.ru>
2023-11-28 17:29:59 +0300

    added file tree and creating relations

--- src/components/NetworkFamili.vue
+++ src/components/NetworkFamili.vue
@@ -6,25 +6,58 @@
 <script setup>
 import { DataSet, Network } from 'vis-network/standalone'
 import { onMounted, ref } from 'vue'
+import { tree } from '@/tree.js'
 
 const canvasElement = ref()
 
+console.log(tree)
+
 onMounted(() => {
-  const nodes = new DataSet([
-    { id: 1, label: 'diamonds1', shape: 'image', image: './src/assets/1.jpg' },
-    { id: 2, label: 'diamonds2', shape: 'image', image: './src/assets/2.jpg' },
-    { id: 3, label: 'diamonds3', shape: 'image', image: './src/assets/1.jpg' },
-    { id: 4, label: 'diamonds4', shape: 'image', image: './src/assets/1.jpg' },
-    { id: 5, label: 'diamonds5', shape: 'image', image: './src/assets/1.jpg' }
-  ])
+  const nodes = new DataSet([])
 
   // create an array with edges
-  var edges = [
-    { from: 1, to: 2 },
-    { from: 2, to: 1 },
-    { from: 2, to: 3 },
-    { from: 2, to: 4 }
-  ]
+  const edges = new DataSet([])
+
+  tree.Individuals.forEach((element) => {
+    nodes.add({
+      id: element.Id,
+      label: element.Fullname,
+      shape: 'image',
+      image: element.Sex === 'F' ? '/2.jpg' : '/1.jpg',
+      level: 0
+    })
+  })
+
+  window.nodes = nodes
+
+  tree.Relations.forEach((relation, index) => {
+    nodes.update([
+      {
+        id: index.Husband,
+        level: index
+      },
+      {
+        id: relation.Wife,
+        level: index
+      }
+    ])
+
+    if (Array.isArray(relation.Children)) {
+      relation.Children.forEach((childId, childrenIndex) => {
+        nodes.update({
+          id: childId,
+          level: index + 1
+        })
+
+        edges.add({ id: `${relation.Id}-${childrenIndex}-H`, from: relation.Husband, to: childId })
+        edges.add({ id: `${relation.Id}-${childrenIndex}-W`, from: relation.Wife, to: childId })
+      })
+    }
+    // else {
+    //   edges.add({ id: `${relation.Id}-H`, from: relation.Husband, to: relation.Children })
+    //   edges.add({ id: `${relation.Id}-W`, from: relation.Wife, to: relation.Children })
+    // }
+  })
 
   // create a network
   const container = document.getElementById('mynetwork')
@@ -50,5 +83,29 @@ onMounted(() => {
   }
 
   const network = new Network(container, data, options)
+
+  network.on('beforeDrawing', function (ctx) {
+    ctx.strokeStyle = '#ff3333'
+    ctx.lineWidth = 1
+    ctx.beginPath()
+    ctx.moveTo(0, 0)
+
+    for (const Node1 of nodes.get()) {
+      var Pos1 = network.getPositions([Node1.id])[Node1.id]
+
+      for (const Node2 of network.getConnectedNodes(Node1.id, 'to')) {
+        var Pos2 = network.getPositions([Node2])[Node2]
+
+        ctx.moveTo(Pos1.x, Pos1.y)
+        ctx.lineTo(Pos1.x, Pos1.y + 50)
+        ctx.lineTo(Pos2.x, Pos1.y + 50)
+        ctx.lineTo(Pos2.x, Pos2.y)
+      }
+    }
+
+    ctx.moveTo(0, 0)
+    ctx.closePath()
+    ctx.stroke()
+  })
 })
 </script>
